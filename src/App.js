957a7f59bbfe6f09b19efeb56775e5f5a53eed85
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-01-08 22:17:04 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import About from "./pages/About.jsx";
 import ProductView from "./pages/Product_view.jsx";
 import LoginRegister from "./pages/login_reg.jsx";
 import BuyNowForm from "./components/buy_from.jsx";
+import Payment from "./components/Payment.jsx"
+import PaymentSuccess from "./components/pay_success.jsx";
+import Show_orders from "./pages/abmin_show_order.jsx";
+import AdminOrders from "./pages/ad_online_pay.jsx";
 function App() {
   return (
     <Router>
@@ -19,6 +23,10 @@ function App() {
           <Route path="/product/:id" element={<ProductView />} />
           <Route path="/auth" element={<LoginRegister />} />
           <Route path="/buy" element={<BuyNowForm />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/success" element={<PaymentSuccess />} />
+          <Route path="/show_orders" element={<Show_orders />} />
+          <Route path="/online_payment" element={<AdminOrders />} />
 
         </Routes>
       </div>
